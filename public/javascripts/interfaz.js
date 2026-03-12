ca573b4fe7f6d16c1b3d59b4237221496d434ad1
javi <javier.s408034@cesurformacion.com>
2026-03-13 00:08:26 +0100

    modal login/registro funcional

--- public/javascripts/interfaz.js
+++ public/javascripts/interfaz.js
@@ -16,6 +16,22 @@ const btnCerrarModal = document.querySelector('#btn-cerrar-modal');
 const listaHistorial = document.querySelector('#lista-historial');
 const templateTarjeta = document.querySelector('#template-tarjeta-aparcamiento');
 
+// ==========================================
+// ELEMENTOS DEL DOM (Modal Login/Registro)
+// ==========================================
+const btnAbrirLogin = document.querySelector('#btn-login'); //menu lateral
+const modalLogin = document.querySelector('#modal-login');
+const modalLoginBackdrop = document.querySelector('#modal-login-backdrop');
+const btnCerrarLogin = document.querySelector('#btn-cerrar-login');
+
+// Elementos internos del Modal (Tabs y Formularios)
+const tabLogin = document.querySelector('#tab-login');
+const tabRegistro = document.querySelector('#tab-registro');
+const formLogin = document.querySelector('#form-login');
+const formRegistro = document.querySelector('#form-registro');
+const tituloModalLogin = document.querySelector('#titulo-modal-login');
+const alertaAuth = document.querySelector('#alerta-auth');
+
 // ===================
 // FUNCIONES DEL MENÚ  
 // ===================
@@ -64,6 +80,7 @@ function abrirModalHistorial() {
     modalHistorial.classList.add('opacity-100', 'scale-100');
 }
 
+// Funcion para cerrar el modal del historial de aparcamientos
 function cerrarModalHistorial() {
 
     // ocultar backdrop del modal
@@ -74,6 +91,74 @@ function cerrarModalHistorial() {
     modalHistorial.classList.remove('opacity-100', 'scale-100');
     modalHistorial.classList.add('opacity-0', 'pointer-events-none', 'scale-95');
 
+}
+
+// ==========================================
+// FUNCIONES DEL MODAL DE LOGIN / REGISTRO
+// ==========================================
+
+// abrir modal de login/registro
+function abrirModalLogin() {
+
+    cerrarMenu(); 
+    
+    mostrarTabLogin(); // mostrar login por defecto
+
+    // backdrop del modal
+    modalLoginBackdrop.classList.remove('opacity-0', 'pointer-events-none');
+    modalLoginBackdrop.classList.add('opacity-100');
+
+    // tarjeta del modal
+    modalLogin.classList.remove('opacity-0', 'pointer-events-none', 'scale-95');
+    modalLogin.classList.add('opacity-100', 'scale-100');
+}
+
+// cerrar modal de login/registro
+function cerrarModalLogin() {
+
+    modalLoginBackdrop.classList.remove('opacity-100');
+    modalLoginBackdrop.classList.add('opacity-0', 'pointer-events-none');
+
+    modalLogin.classList.remove('opacity-100', 'scale-100');
+    modalLogin.classList.add('opacity-0', 'pointer-events-none', 'scale-95');
+}
+
+// cambiar pestaña registro -> login
+function mostrarTabLogin() {
+    
+    formLogin.classList.remove('hidden');
+    formRegistro.classList.add('hidden');
+    
+    tituloModalLogin.textContent = 'Bienvenido';
+
+    alertaAuth.classList.add('hidden'); // ocultamos las posibles alertas
+
+    //pestaña login
+    tabLogin.classList.add('bg-white', 'text-brand-primary', 'shadow-sm', 'font-bold');
+    tabLogin.classList.remove('text-gray-500', 'hover:text-gray-700', 'font-medium');
+
+    // pestaña registro
+    tabRegistro.classList.remove('bg-white', 'text-brand-primary', 'shadow-sm', 'font-bold');
+    tabRegistro.classList.add('text-gray-500', 'hover:text-gray-700', 'font-medium');
+}
+
+// cambiar pestaña login -> registro
+function mostrarTabRegistro() {
+    
+    formRegistro.classList.remove('hidden');
+    formLogin.classList.add('hidden');
+    
+    tituloModalLogin.textContent = 'Crear Cuenta';
+
+    alertaAuth.classList.add('hidden');
+
+    // pestaña registro
+    tabRegistro.classList.add('bg-white', 'text-brand-primary', 'shadow-sm', 'font-bold');
+    tabRegistro.classList.remove('text-gray-500', 'hover:text-gray-700', 'font-medium');
+
+    // pestaña login
+    tabLogin.classList.remove('bg-white', 'text-brand-primary', 'shadow-sm', 'font-bold');
+    tabLogin.classList.add('text-gray-500', 'hover:text-gray-700', 'font-medium');
 }
 
 // ===========
@@ -214,3 +299,18 @@ listaHistorial.addEventListener('click', async (e) => {
     }
 
 });
+
+// click boton mostrar login/registro
+btnAbrirLogin.addEventListener('click', abrirModalLogin);
+
+// click cerrar modal login/registro "X"
+btnCerrarLogin.addEventListener('click', cerrarModalLogin);
+
+// click fuera del modal de login/registro (en el backdrop)
+modalLoginBackdrop.addEventListener('click', cerrarModalLogin);
+
+// tab pestaña login
+tabLogin.addEventListener('click', mostrarTabLogin);
+
+// tab pestaña registro
+tabRegistro.addEventListener('click', mostrarTabRegistro);
