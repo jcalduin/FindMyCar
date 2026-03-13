57d3bb4802d274a96f2ac5199701b4c911c17346
javi <javier.s408034@cesurformacion.com>
2026-03-13 03:31:24 +0100

    rutas para sesiones

---
+++ routes/auth.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+const authController = require('../controllers/auth.controller');
+
+//procesar el formulario de Registro
+router.post('/registro', authController.registro);
+
+//procesar el formulario de Login
+router.post('/login', authController.login);
+
+//cerrar sesión
+router.post('/logout', authController.logout);
+
+module.exports = router;
