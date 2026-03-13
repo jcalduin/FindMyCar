0c6a8e433e9351ac28d64f298cf5f3c21256fe31
jcalduin <cabreramirandajavier@gmail.com>
2026-03-13 11:35:53 +0100

    aparcamiento-model coompleto

---
+++ models/aparcamiento-model.js
@@ -0,0 +1,48 @@
+const db = require('../database/db');
+
+class AparcamientoModel {
+
+    // Guarda solo las cordenadas y la hora de inicio, el resto se actualiza al finalizar el aparcamiento
+    static crearAparcamiento(usuarioId, latitud, longitud, tiempoInicio) {
+
+        const sql = `
+            INSERT INTO aparcamientos (usuario_id, latitud, longitud, inicio) 
+            VALUES (?, ?, ?, ?)
+        `;
+
+        const info = db.prepare(sql).run(usuarioId, latitud, longitud, tiempoInicio);
+        return info.lastInsertRowid; // Devolvemos el ID del aparcamiento recién creado
+
+    }
+
+    // finalizar aparcamiento actualiza la entrada con los datos que faltan
+    static finalizarAparcamiento(aparcamientoId, tiempoFin, minutosTotales) {
+
+        const sql = `
+            UPDATE aparcamientos 
+            SET fin = ?, tiempoTotalMinutos = ? 
+            WHERE id = ?
+        `;
+
+        return db.prepare(sql).run(tiempoFin, minutosTotales, aparcamientoId);
+
+    }
+
+    // miramos si el hay un parking es decir si fin es null.
+    static obtenerActivoPorUsuario(usuarioId) {
+        
+        const sql = 'SELECT * FROM aparcamientos WHERE usuario_id = ? AND fin IS NULL'; // si esxiste un registro con fin null significa que el parking esta activo
+        return db.prepare(sql).get(usuarioId);
+
+    }
+
+    // obtenemos el historial del usuario ordenados por el mas reciente para mostrarlo en l interfaz
+    static obtenerHistorialPorUsuario(usuarioId) {
+        
+        const sql = 'SELECT * FROM aparcamientos WHERE usuario_id = ? AND fin IS NOT NULL ORDER BY inicio DESC';
+        return db.prepare(sql).all(usuarioId);
+        
+    }
+}
+
+module.exports = AparcamientoModel;
